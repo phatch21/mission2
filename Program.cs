9226fe7afe8badbe3b51966452dff49195f05e4e
phatch21 <phatch21@byu.edu>
2025-01-15 14:08:09 -0700

    setup project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int numThrows = 0;
+
+        Console.WriteLine("Welcome to the dice throwing simulator!\nHow many dice rolls would you like to simulate?");
+        numThrows = int.Parse(Console.ReadLine());
+    }
+
+
+}
