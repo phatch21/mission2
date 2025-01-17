7a6e8c23a4fea25cb4eae7122f5920735b50e207
phatch21 <phatch21@byu.edu>
2025-01-17 13:53:23 -0700

    finished comments

--- DiceRollingSimulator.cs
+++ DiceRollingSimulator.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// Payton Hatch
+// Group 4-6
 
+// namespace of the project name for reference
 namespace mission2
-{
-    class DiceSimulator
-    {
+
+{   // second class
+    internal class DiceSimulator
+    {   
+        // random object created
         private Random _random = new Random();
 
+        //method to calculate each roll
         public int[] SimulateRolls(int numRolls)
         {
-            int[] rollCounts = new int[13]; // Index 0 and 1 will not be used.
+            // creates an array in which index 0 and 1 will not be used.
+            int[] rollCounts = new int[13]; 
 
+            // for loop that iterates through the number of rolls passed to the method
             for (int i = 0; i < numRolls; i++)
             {
-                int dice1 = _random.Next(1, 7);
+                // random number for each die between 1 and 6 since Next() is not inclusive
+                int dice1 = _random.Next(1, 7); 
                 int dice2 = _random.Next(1, 7);
                 int sum = dice1 + dice2;
                 rollCounts[sum]++;
             }
 
+            // returns the calculated array
             return rollCounts;
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,32 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Payton Hatch
+// Group 4-6
 
+// using statement connects this program with the rest of the project
 using mission2;
 internal class Program
 {
+    // creates constructor method
     static void Main(string[] args)
     {
+        int numRolls = 0;
+
+        // introduction and query for number of rolls
         Console.WriteLine("Welcome to the dice throwing simulator!");
         Console.Write("How many dice rolls would you like to simulate? ");
 
-        if (!int.TryParse(Console.ReadLine(), out int numRolls) || numRolls <= 0)
-        {
-            Console.WriteLine("Please enter a valid positive integer.");
-            return;
-        }
+        // stores number of rolls entered by the user
+        numRolls = int.Parse(Console.ReadLine());
 
+        // creates object from the DiceSimulator() method of the DiceRollingSimulator class
         DiceSimulator simulator = new DiceSimulator();
         int[] rollResults = simulator.SimulateRolls(numRolls);
 
+        // message describing simulation results
         Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         Console.WriteLine("Total number of rolls = " + numRolls + ".\n");
 
-        for (int i = 2; i <= 12; i++)
-        {
-            int percentage = (int)Math.Round((double)rollResults[i] * 100 / numRolls);
-            Console.WriteLine(i + ": " + new string('*', percentage));
+        // for loop creating the histogram according to roll count
+        for (int iCount = 2; iCount <= 12; iCount++)
+        {   
+            // calculates the percentage each count takes of the total number of rolls and prints it
+            int percentage = (int)Math.Round((double)rollResults[iCount] * 100 / numRolls);
+            Console.WriteLine(iCount + ": " + new string('*', percentage));
         }
 
+        // final message
         Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
     }
 }
