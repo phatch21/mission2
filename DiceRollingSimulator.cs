eea60e76d872f1403359d33cb36c2164dddbc02c
phatch21 <phatch21@byu.edu>
2025-01-17 13:28:40 -0700

    pull

---
+++ DiceRollingSimulator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission2
+{
+    class DiceSimulator
+    {
+        private Random _random = new Random();
+
+        public int[] SimulateRolls(int numRolls)
+        {
+            int[] rollCounts = new int[13]; // Index 0 and 1 will not be used.
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                int dice1 = _random.Next(1, 7);
+                int dice2 = _random.Next(1, 7);
+                int sum = dice1 + dice2;
+                rollCounts[sum]++;
+            }
+
+            return rollCounts;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using mission2;
 internal class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        int numThrows = 0;
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.Write("How many dice rolls would you like to simulate? ");
 
-        Console.WriteLine("Welcome to the dice throwing simulator!\nHow many dice rolls would you like to simulate?");
-        numThrows = int.Parse(Console.ReadLine());
+        if (!int.TryParse(Console.ReadLine(), out int numRolls) || numRolls <= 0)
+        {
+            Console.WriteLine("Please enter a valid positive integer.");
+            return;
+        }
+
+        DiceSimulator simulator = new DiceSimulator();
+        int[] rollResults = simulator.SimulateRolls(numRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + numRolls + ".\n");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = (int)Math.Round((double)rollResults[i] * 100 / numRolls);
+            Console.WriteLine(i + ": " + new string('*', percentage));
+        }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
     }
-
-
 }
+
+
